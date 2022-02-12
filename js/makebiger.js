744e4edbe2a0b1f6b4618c117c85271eeb73d0f9
imenesg <imenesgabriel@gmail.com>
2022-02-12 20:32:57 -0300

    volta a ser como antes

---
+++ js/makebiger.js
@@ -0,0 +1,14 @@
+/*var projeto = document.querySelectorAll(".projeto");
+console.log("inicio");
+
+for (let i = 0; i < projeto.length; i++) {
+    projeto[i].addEventListener("click", makebiger);
+}
+
+function makebiger(){
+    let screen = document.querySelector(".screen")
+    console.log(this);
+    this.classList.toggle("biger");
+    screen.classList.toggle("darker");
+    console.log(this.classList);
+}*/
